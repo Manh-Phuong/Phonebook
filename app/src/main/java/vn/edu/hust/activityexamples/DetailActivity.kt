8c0b7559b6b35c272aba47077ffdd728628a9ba6
Manh-Phuong <phuong.nm23112002@gmail.com>
2023-11-16 22:54:39 +0700

    update

--- app/src/main/java/vn/edu/hust/activityexamples/DetailActivity.kt
+++ app/src/main/java/vn/edu/hust/activityexamples/DetailActivity.kt
@@ -1,9 +1,11 @@
 package vn.edu.hust.activityexamples
 
 import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +18,12 @@ class DetailActivity : AppCompatActivity() {
         val phone = findViewById<TextView>(R.id.phone)
         val email = findViewById<TextView>(R.id.email)
         findViewById<TextView>(R.id.text_user_name).text = user?.fullName?.get(0).toString()
+
+        val backgroundColors = listOf("#5a99fa", "#fb8c67", "#9acd68", "#92a3ad", "#8C33FF")
+        val color = user?.idUser?.toInt()?.let { Color.parseColor(backgroundColors[(it-1) % backgroundColors.size]) }
+
+        val colorValue = color ?: Color.TRANSPARENT
+        findViewById<CardView>(R.id.card_view_2).setCardBackgroundColor(colorValue)
 
         id.text = "ID: ${user?.idUser.toString()}"
         name.text = "Họ và tên: ${user?.fullName.toString()}"
